3f9563b5b15d1ad4811f978e4f9924962d78cece
Mans Rullgard <mans@mansr.com>
2018-10-11 16:07:46 +0100

    usb: set of_node for per-interface devices

--- drivers/usb/core/of.c
+++ drivers/usb/core/of.c
@@ -148,3 +148,14 @@ usb_of_get_interface_node(struct usb_device *udev, u8 config, u8 ifnum)
 	return NULL;
 }
 EXPORT_SYMBOL_GPL(usb_of_get_interface_node);
+
+int usb_of_get_removable(struct device_node *node)
+{
+	u32 removable;
+
+	if (!of_property_read_u32(node, "removable", &removable))
+		return removable ? USB_DEVICE_REMOVABLE : USB_DEVICE_FIXED;
+
+	return -ENOENT;
+}
+EXPORT_SYMBOL_GPL(usb_of_get_removable);
